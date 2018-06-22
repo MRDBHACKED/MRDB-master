d51c50567abf8492801b314e9d91349b2a4451c0
MRDBHACKED <34579976+MRDBHACKED@users.noreply.github.com>
2018-06-22 12:40:30 -0700

    Add files via upload

---
+++ MRDB master/MRDB.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+banner = '''
+
+           /'        /|
+           /         / |_
+          /         /  //|
+         /_________/  ////|
+        |   _ _    | 8o////|
+        | /'// )_  |   8///|
+        |/ // // ) |   8o///|
+        / // // //,|  /  8//|
+       / // // /// | /   8//|  Coded By MRDBPS-HACKER
+      / // // ///__|/    8//|
+     /.(_)// /// |       8///|
+    (_)' `(_)//| |       8////|___________
+   (_) /_\ (_)'| |        8///////////////
+   (_) \"/ (_)'|_|         8/////////////
+    (_)._.(_) d' Hb         8oooooooopb'
+      `(_)'  d'  H`b
+            d'   `b`b
+           d'     H `b
+          d'      `b `b
+         d'           `b
+        d'             `b   
+                            \033[91m[coded by DBPS@HACKER]
+                          
+                          https://www.facebook.com/xXO.oXxD
+'''
+print banner
+import os
+print ('\033[92m[1].Admin-Finder')
+print ('\033[92m[2]..Check-server')
+print ('\033[92m[3]...server_ip-scan')
+print ('\033[92m[4]....Dorks')
+print ('\033[92m[5].....wordepress')
+print ('\033[92m[6]......TNT')
+print ('\033[92m[7].......XAttacker-master')
+print ('\033[92m[8]........SpyV5therevo')
+print ('\033[92m[9].........priv8 auto Exploiter')
+print ('\033[92m[10].........Sql Injection')
+choose = raw_input('\033[93m \n [+]>Choose a number <[+] >>>>:')
+if choose =='1':
+ os.system('git clone https://github.com/DBPS666/Admin-Finder.git')
+if choose =='2':
+ os.system('git clone https://github.com/DBPS666/Check-server.git')
+if choose =='3':
+ os.system ('git clone https://github.com/DBPS666/server_ip-scan.git')
+if choose =='4':
+  ('https://github.com/DBPS666/Dorks.git')
+if choose =='5':
+ os.system ('git clone https://github.com/DBPS666/wordepress.git')
+if choose =='6':
+ os.system ('git clone https://github.com/DBPS666/TNTUP.git')
+if choose =='7':
+ os.system ('git clone https://github.com/DBPS666/XAttacker-master.git')
+if choose =='8':
+ os.system ('git clone https://github.com/DBPS666/SpyV5therevo2.git')
+if choose =='9':
+ os.system ('git clone https://github.com/DBPS666/priv8-auto-Exploiter.git')
+if choose =='10':
+ print ('git clone https://github.com/DBPS666/Sql-Injection-.git')
+else:
+ print ('\n \033[91m   ok .|. -_- .|. ')
---
+++ MRDB master/README.md
@@ -0,0 +1,7 @@
+#DBPS@HACKER
+Ahmed Albelbisi
+https://www.facebook.com/xXO.oXxD
+language:python
+17/6/2018
+2:05pm
+Amman-jordan
